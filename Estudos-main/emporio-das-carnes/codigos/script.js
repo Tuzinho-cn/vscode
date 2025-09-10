a090ad245c56fe043ebbae06f62096f3bb77ad96
Arthur <arthurcn614@gmail.com>
2025-09-10 14:38:42 -0300

    atualizando minahas coisas no git

---
+++ Estudos-main/emporio-das-carnes/codigos/script.js
@@ -0,0 +1,14 @@
+
+var usuário = 
+
+function enviar() {
+   
+}
+
+function clickMenu() {
+   if (itens.style.display == 'block') {
+      itens.style.display = 'none'
+   } else {
+      itens.style.display = 'block'
+   }
+}
