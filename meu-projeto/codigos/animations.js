3c9670fe631a91e19b1ba32fa58ed590b286b66c
Arthur <arthurcn614@gmail.com>
2025-09-17 11:07:17 -0300

    Commitando pra iniciar outro projeto

--- meu-projeto/codigos/animations.js
+++ meu-projeto/codigos/animations.js
@@ -1,24 +1,15 @@
-function join() {
-   let start = Date.now(); // remember start time
-
-   let timer = setInterval(function() {
-     // how much time passed from the start?
-     let timePassed = Date.now() - start;
-   
-     if (timePassed >= 2000) {
-       clearInterval(timer); // finish the animation after 2 seconds
-       return;
-     }
-   
-     // draw the animation at the moment timePassed
-     draw(timePassed);
-   
-   }, 20);
-   
-   // as timePassed goes from 0 to 2000
-   // left gets values from 0px to 400px
-   function draw(timePassed) {
-     train.style.left = timePassed / 5 + 'px';
- }
+function clickMenu() {
+    if (itens.style.display == 'block') {
+      itens.style.display = 'none'
+    } else {
+      itens.style.display = 'block'
+    }
 }
 
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+      itens.style.display = 'block'
+    } else {
+      itens.style.display = 'none'
+    }
+}
