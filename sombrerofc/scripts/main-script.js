7b5f747010ecdacb4ae47e5d51230dd11dcf7260
Arthur <arthurcn614@gmail.com>
2025-09-22 13:15:14 -0300

    fiz a tabela de pontuação do úlitimo jogo

--- sombrerofc/scripts/main-script.js
+++ sombrerofc/scripts/main-script.js
@@ -15,3 +15,20 @@ function mudouTamanho() {
     account_circle.style.display = 'none'
     }
 }
+
+// Obter o elemento HTML pelo ID
+const elementoData = document.getElementById('data-atual');
+
+// Criar um novo objeto Date para obter a data e hora atuais
+const dataAtual = new Date();
+
+// Obter os componentes da data
+const dia = dataAtual.getDate();
+const mes = dataAtual.getMonth() + 1; // Adicionar 1, pois os meses são baseados em zero (0-11)
+const ano = dataAtual.getFullYear();
+
+// Construir a string da data (ex: 20/09/2025)
+const dataFormatada = `${dia}/${mes}/${ano}`;
+
+// Inserir a data formatada no elemento HTML
+elementoData.textContent = `(${dataFormatada})`;
