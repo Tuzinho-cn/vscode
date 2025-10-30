96d0921d09bc381bb90c95082b830b81c273df02
Arthur Correia Neves <arthurcn614@gmail.com>
2025-10-29 21:53:28 -0300

    commitando novo desafio de site
    Signed-off-by: Arthur Correia Neves <arthurcn614@gmail.com>

--- aulas/projetos-desafios/javascript/desafios/employees-list/scripts/main-script.js
+++ aulas/projetos-desafios/javascript/desafios/employees-list/scripts/main-script.js
@@ -38,8 +38,8 @@ function Show(type)
    {
       case 's-home':
          home.style.display = 'block';
-         bhome.style.background = '#FFFFFF'
-         ihome.style.color = '#5d4181'
+         bhome.style.setProperty('background', '#FFFFFF', 'important')
+         ihome.style.setProperty('color', '#5d4181', 'important');
 
          stats.style.display = 'none';
          bstats.style.background = 'transparent'
@@ -64,8 +64,8 @@ function Show(type)
          ihome.style.color = '#FFFFFF'
 
          stats.style.display = 'block';
-         bstats.style.background = '#FFFFFF'
-         istats.style.color = '#5d4181'
+         bstats.style.setProperty('background', '#FFFFFF', 'important')
+         istats.style.setProperty('color', '#5d4181', 'important');
 
          favv.style.display = 'none';
          bfavv.style.background = 'transparent'
@@ -90,8 +90,8 @@ function Show(type)
          istats.style.color = '#FFFFFF'
 
          favv.style.display = 'block';
-         bfavv.style.background = '#FFFFFF'
-         ifavv.style.color = '#5d4181'
+         bfavv.style.setProperty('background', '#FFFFFF', 'important')
+         ifavv.style.setProperty('color', '#5d4181', 'important');
 
          empl.style.display = 'none';
          bempl.style.background = 'transparent'
@@ -116,8 +116,8 @@ function Show(type)
          ifavv.style.color = '#FFFFFF'
 
          empl.style.display = 'block';
-         bempl.style.background = '#FFFFFF'
-         iempl.style.color = '#5d4181'
+         bempl.style.setProperty('background', '#FFFFFF', 'important')
+         iempl.style.setProperty('color', '#5d4181', 'important');
 
          sett.style.display = 'none';
          bsett.style.background = 'transparent'
@@ -142,8 +142,112 @@ function Show(type)
          iempl.style.color = '#FFFFFF'
 
          sett.style.display = 'block';
-         bsett.style.background = '#FFFFFF'
-         isett.style.color = '#5d4181'
+         bsett.style.setProperty('background', '#FFFFFF', 'important')
+         isett.style.setProperty('color', '#5d4181', 'important');
       break;
    }
 }
+
+//--------------------
+
+let bacts = document.getElementById("bactions");
+
+let bactp = document.getElementById('bactionp');
+
+function Pressed(which)
+{
+   if (which == 'bsec')
+   {
+         bacts.style.setProperty('background', '#FFFFFF', 'important')
+         bacts.style.setProperty('color', '#5d4181', 'important');
+
+         bactp.style.background = '#7351A1'
+         bactp.style.color = '#FFFFFF'
+   }
+   else if (which == 'bpri')
+   {
+         bacts.style.background = '#7351A1'
+         bacts.style.color = '#FFFFFF'
+
+         bactp.style.setProperty('background', '#FFFFFF', 'important')
+         bactp.style.setProperty('color', '#5d4181', 'important');
+   }
+}
+
+//--------------------
+
+let flist = document.getElementById('filter-list');
+
+function showFilters()
+{
+   if (flist.style.display != 'block')
+   {
+      flist.style.display = 'block';
+   }
+   else
+   {
+      flist.style.display = 'none';
+   }
+}
+
+//------------------------
+
+//PRECISO FAZER ESSA MERDA DE FILTRO FUNFAR LEGALZIN
+
+//#region FILTRO
+//-------------------
+
+// let cfema = document.getElementById('cfema');
+
+// let cmale = document.getElementById('cmale');
+
+// let cfive = document.getElementById('cfive');
+
+// let listsexArray = [];
+// let listtrArray = [];
+
+// listsexArray.push(document.getElementById('1').textContent);
+// listsexArray.push(document.getElementById('3').textContent);
+// listsexArray.push(document.getElementById('2').textContent);
+// listsexArray.push(document.getElementById('4').textContent);
+// listsexArray.push(document.getElementById('5').textContent);
+// listsexArray.push(document.getElementById('6').textContent);
+
+
+
+// listtrArray.push(document.getElementById('tr1'));
+// listtrArray.push(document.getElementById('tr2'));
+// listtrArray.push(document.getElementById('tr3'));
+// listtrArray.push(document.getElementById('tr4'));
+// listtrArray.push(document.getElementById('tr5'));
+// listtrArray.push(document.getElementById('tr6'));
+
+// let i = 1;
+
+// let maxlisttrArray = listtrArray.length;
+
+// function Filter()
+// {
+    let test = document.getElementById('test');
+//       for (i = 1 ; i <= maxlisttrArray ; i++)
+//       {
+//          if (listsexArray[i] != "Female")
+//          {
+//             listtrArray[i].style.display = 'none';
+//          }
+//          else
+//          {
+//             if (listtrArray[i].style.display == 'none')
+//             {
+//                test.innerHTML = "funfou";
+//             }
+//          }
+//       }
+// }
+
+//#endregion
+
+//-------------------------
+
+
+
